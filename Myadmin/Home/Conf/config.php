77c464a6695106e3d832ae241cd9824071442c89
zhangxinglong <zhangxinglong@gbase.cn>
2016-12-12 16:41:18 +0800

    登陆后台管理页面需要经过ip白名单校验

--- Myadmin/Home/Conf/config.php
+++ Myadmin/Home/Conf/config.php
@@ -1,5 +1,16 @@
 <?php
-if (is_file('./Conf/common.php'))
-    return array_merge(require_once('./Conf/common.php'), array(
+$common_config_file = './Conf/common.php';
+// 文件存在的情况下，is_file比file_exists要快N倍
+if (is_file($common_config_file)) {
+    $common_conf = load_config($common_config_file, 'php');
+    
+    $ipWhitelistFile = './Conf/adminipWhitelist.php';
+    $iplist = array();
+    if (is_file($ipWhitelistFile)) {
+        $iplist = load_config($ipWhitelistFile, 'php');
+    }
+
+    $config = array_merge($common_conf, $iplist, array(
         'URL_MODEL' => 1,    // PATHINFO模式
     ));
+}
